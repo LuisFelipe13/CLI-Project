10328ae4940ebffeab0367c7bb8d08347fb4c36d
Luis del Cueto <luisdelcueto@live.com>
2017-05-15 10:46:40 -0400

    basically finito

--- lib1/cli.rb
+++ lib1/cli.rb
@@ -1,18 +1,89 @@
 class CLI
+  @@doh = Artii::Base.new :font => 'doh'
+  @@calgphy2 = Artii::Base.new :font => 'calgphy2'
+  @@slant = Artii::Base.new :font => 'slant'
+  @@doom =  Artii::Base.new :font => 'doom'
   def welcome
-    puts "Welcome to TweetTrend, the only command line app you'll ever need to see what's trending!"
-    puts "Enter a twitter topic (for example, politics) and I'll be more than happy to show you what's trending:"
+    #Welcomes user and asks for twitter topic
+    system("clear")
+    puts @@doh.asciify('       Welcome To').red
+    puts @@doh.asciify('    TweetTrend').light_cyan
+    puts @@slant.asciify('                                        Click Enter To Continue').red.blink
+    puts ("                                                                                                          #########################################################")
+    puts ("                                                                                                          #########################################################")
+    puts ("                                                                                                          ###               Welcome to TweetTrend©              ###")
+    puts ("                                                                                                          ###                                                   ###")
+    puts ("                                                                                                          ###    The only command line app you'll ever need     ###")
+    puts ("                                                                                                          ###" + "               TO SEE WHATS TRENDING!".red.blink + "              ###")
+    puts ("                                                                                                          ###                                                   ###")
+    puts ("                                                                                                          ###                                                   ###")
+    puts ("                                                                                                          ###     Enter 'quit' at any time to exit the app      ###")
+    puts ("                                                                                                          #########################################################")
+    puts ("                                                                                                          #########################################################")
     response = gets.chomp
-    tweet_client = TwitterApi.new
-    puts tweet_client.search(response)
-    continue
+    exit if response == "quit"
+    user_input
+    # puts ("###            Enter any twitter topic,      ##")
+    # puts ("###                  'Politics'                        ###")
+    # puts ("and I'll be more than happy to show you what's trending:    ")
   end
 
-  def continue
-    puts "^ Tweets ^ Above ^ \n Enter a new topic to see what's trending:"
+  def user_input
+    system("clear")
+    puts @@slant.asciify("Enter A Topic Now :").magenta
+    topic = gets.chomp
+    topic = topic.downcase
+    exit if topic == "quit"
+    #Checks if user would like to specify a location
+    puts @@slant.asciify("Would you also like to specify a location?  ' Yes ' or ' No '").light_cyan
+    puts "This would allow you to see the treding tweets for your topic specifically from the users located within the given area.".yellow
     response = gets.chomp
-    tweet_client = TwitterApi.new
-    puts tweet_client.search(response)
-    continue
+    response = response.downcase
+    exit if response == "quit"
+    answer = [topic, response]
+    check_answer(answer)
+  end
+
+  def check_answer(answer)
+    if answer[1] == "yes" || answer[1] == "no"
+      search(answer)
+    else
+      puts @@slant.asciify("Please reply with only 'Yes' or 'No' ").red
+      response = gets.chomp
+      response = response.downcase
+      exit if response == "quit"
+      answer[1] = response
+      check_answer(answer)
+    end
+  end
+
+  def search(answer)
+    if answer[1] == "no"
+      system("clear")
+      puts @@slant.asciify("The TweetTrend Top 10 Tweets").light_cyan
+      tweet_client = TwitterApi.new
+      tweet_client.search(answer[0])
+      puts @@slant.asciify("Click Enter To Continue").red.blink
+      puts @@slant.asciify("OR")
+      puts @@slant.asciify("Type Quit To Exit").blue.blink
+      gets.chomp
+      welcome
+    else
+      puts @@slant.asciify("Please Specify The Location Now :").green
+      puts "Locations must not be large land mases like Countries OR States, \nThey must be cities, towns, or a famous place like the 'Eiffel Tower'".yellow
+      location = gets.chomp
+      location = Geocoder.search(location)[0].coordinates
+      exit if location == "quit"
+      system("clear")
+      puts @@slant.asciify("The TweetTrend Top 10 Tweets").light_cyan
+      tweet_client = TwitterApi.new
+      tweet_client.specific_search(answer[0], location)
+      puts @@slant.asciify("Click Enter To Continue").red.blink
+      puts @@slant.asciify("OR")
+      puts @@slant.asciify("Type Quit To Exit").blue.blink
+      response = gets.chomp
+      exit if response == "quit"
+      welcome
+    end
   end
 end
--- lib1/twitter.rb
+++ lib1/twitter.rb
@@ -1,31 +1,35 @@
-# require 'twitter'
-# require 'yaml'
-# require "pry"
-# require "colorize"
+class TwitterApi
+  attr_reader :client
 
-# class TwitterApi
-#   attr_reader :client
-#
-#   def initialize
-#     keys = YAML.load_file('application.yml')
-#     @client = Twitter::REST::Client.new do |config|
-#       config.consumer_key        = keys['CONSUMER_KEY']
-#       config.consumer_secret     = keys['CONSUMER_SECRET']
-#       config.access_token        = keys['ACCESS_TOKEN']
-#       config.access_token_secret = keys['ACCESS_TOKEN_SECRET']
-#     end
-#   end
-#
-#   def search(response)
-#     array = client.search(response, options = {result_type: "popular"}).take(10)
-#     tweets = []
-#     num = 1
-#     array.each do |x|
-#       string = "#{num}. #{x.user.name}".red + " @#{x.user.screen_name}\n".red + " Tweet: #{x.text}".light_cyan + "\n Date/Time: #{x.created_at} \n Retweets: #{x.retweet_count} |" + " Likes: #{x.favorite_count} | Followers: #{x.user.followers_count}"
-#       tweets << string
-#       num += 1
-#     end
-#     tweets = "Sorry that's not a topic we have info on at the moment, please try again" if tweets.size == 0
-#     tweets
-#   end
-# end
+  def initialize
+    keys = YAML.load_file('application.yml')
+    @client = Twitter::REST::Client.new do |config|
+      config.consumer_key        = keys['CONSUMER_KEY']
+      config.consumer_secret     = keys['CONSUMER_SECRET']
+      config.access_token        = keys['ACCESS_TOKEN']
+      config.access_token_secret = keys['ACCESS_TOKEN_SECRET']
+    end
+  end
+
+  def search(topic)
+    array = client.search(topic, options = {result_type: "popular"}).take(10)
+    tweets = parse_tweet_data(array)
+  end
+
+  def specific_search(topic, location)
+    array = client.search(topic, options = {result_type: "recent", geocode: "#{location[0]},#{location[1]},5mi"}).take(10)
+    tweets = parse_tweet_data(array)
+  end
+
+  def parse_tweet_data(array)
+    tweets = []
+    num = 1
+    array.each do |x|
+      string = "#{num}.".red + "#{x.user.name}" + " @#{x.user.screen_name}\n" + " Tweet: " + "#{x.text}" + "\n Date/Time:" + " #{x.created_at} \n" + " Retweets: #{x.retweet_count} |" + " Likes: " + " #{x.favorite_count} | Followers: #{x.user.followers_count}\n  "
+      tweets << string
+      num += 1
+    end
+    tweets = "Sorry that's not trending, please try again\n  ".red if tweets.size == 0
+    puts tweets
+  end
+end
